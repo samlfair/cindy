885d5d42cea128c6f1417234a4cb3e93825c88b7
Sam Littlefair <sam.littlefair@prismic.io>
2022-01-19 00:09:23 +0000

    Disable favicon (throwing 404)

---
+++ build/_app/chunks/stores-0c6e920e.js
@@ -0,0 +1,1 @@
+import{C as o}from"./vendor-1bffe752.js";const r=o({});export{r as u};
--- src/lib/components/seo.svelte
+++ src/lib/components/seo.svelte
@@ -8,5 +8,5 @@
   <title>{title}</title>
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2284%22>{favicon}</text></svg>"/>
+  <!-- <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2284%22>{favicon}</text></svg>"/> -->
 </svelte:head>
